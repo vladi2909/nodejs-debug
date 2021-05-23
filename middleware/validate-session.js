cc18a935d2a2cafa58b8822cba90cf3276b1a44e
vladimir <52894331+vladi2909@users.noreply.github.com>
2021-05-23 12:29:06 +0300

    refactor: change func declar to arrow

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 var User = require('../db').import('../models/user');
 
-module.exports = function (req, res, next) {
+module.exports = (req, res, next) => {
     if (req.method == 'OPTIONS') {
         next();   // allowing options as a method for request
     } else {
